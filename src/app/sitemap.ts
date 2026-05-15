d182cc7a68dd8678cd71884bb9df057a6f45f284
aniektudiesi-hue <aniektudiesi@gmail.com>
2026-05-15 14:26:03 +0530

    Serve main sitemap as custom XML route

--- src/app/sitemap.ts
+++
@@ -1,8 +0,0 @@
-import type { MetadataRoute } from "next";
-import { staticSitemapRoutes } from "@/lib/sitemap-data";
-
-export const revalidate = 3600;
-
-export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  return staticSitemapRoutes();
-}
---
+++ src/app/sitemap.xml/route.ts
@@ -0,0 +1,25 @@
+import { staticSitemapRoutes } from "@/lib/sitemap-data";
+
+export const revalidate = 3600;
+
+export async function GET() {
+  const routes = staticSitemapRoutes();
+
+  return new Response(toXml(routes.map((route) => route.url)), {
+    headers: {
+      "Content-Type": "application/xml; charset=utf-8",
+      "Cache-Control": "public, max-age=3600, stale-while-revalidate=86400",
+    },
+  });
+}
+
+function toXml(urls: string[]) {
+  return `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n${urls
+    .map((url) => `  <url><loc>${escapeXml(url)}</loc></url>`)
+    .join("\n")}\n</urlset>`;
+}
+
+function escapeXml(value: string) {
+  return value.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;");
+}
+
